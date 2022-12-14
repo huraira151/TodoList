8a6f5d2c5db496abea66d1c866567dc51ff81260
huraira151 <raohuraira151@gmail.com>
2022-12-14 14:59:38 +0500

    Updates

---
+++ precache-manifest.a70d5e8082158b893b05199759f46c87.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ba1d09626e4808f6230bef923127712",
+    "url": "/TodoList/index.html"
+  },
+  {
+    "revision": "809ce9cc34d16dc62c58",
+    "url": "/TodoList/static/css/main.c157be19.chunk.css"
+  },
+  {
+    "revision": "2dbc57f0e4f89020a769",
+    "url": "/TodoList/static/js/2.528e987f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/TodoList/static/js/2.528e987f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "809ce9cc34d16dc62c58",
+    "url": "/TodoList/static/js/main.e9f51d96.chunk.js"
+  },
+  {
+    "revision": "b45dd5b29da7de8c8059",
+    "url": "/TodoList/static/js/runtime-main.7e8cf876.js"
+  }
+]);
