64db03fa673b10557913bace192ab162b8da5bc4
huraira151 <raohuraira151@gmail.com>
2022-12-15 10:46:53 +0500

    Fourth Commit

---
+++ build/precache-manifest.4aea7a4f2e3e99285a9b0c792502e920.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bac4500fccea59d2070a5d6a661a2d25",
+    "url": "/TodoList/index.html"
+  },
+  {
+    "revision": "0e0346067937462d45aa",
+    "url": "/TodoList/static/css/main.91a6bd25.chunk.css"
+  },
+  {
+    "revision": "8af23a8cc647b1cd1252",
+    "url": "/TodoList/static/js/2.97bffe46.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/TodoList/static/js/2.97bffe46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e0346067937462d45aa",
+    "url": "/TodoList/static/js/main.a03d9c6e.chunk.js"
+  },
+  {
+    "revision": "b45dd5b29da7de8c8059",
+    "url": "/TodoList/static/js/runtime-main.7e8cf876.js"
+  }
+]);
